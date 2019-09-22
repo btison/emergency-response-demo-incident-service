90f88c2182e4286a1a70e56a41351f04dcd14912
Bernard Tison <bernard.tison@gmail.com>
2019-09-22 09:33:59 +0200

    Add jaeger instrumentation to Kafka producer
    Conflicts:
    	pom.xml

--- src/main/java/com/redhat/cajun/navy/incident/KafkaConfiguration.java
+++ src/main/java/com/redhat/cajun/navy/incident/KafkaConfiguration.java
@@ -4,10 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.redhat.cajun.navy.incident.message.Message;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.kafka.spring.TracingProducerFactory;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,6 +30,9 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @EnableKafka
 public class KafkaConfiguration {
 
+    @Autowired
+    private Tracer tracer;
+
     @Value(value = "${kafka.bootstrap-address}")
     private String bootstrapAddress;
 
@@ -42,7 +48,7 @@ public class KafkaConfiguration {
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
+        return new TracingProducerFactory<String, Message<?>>(new DefaultKafkaProducerFactory<>(configProps));
     }
 
     @Bean(name = "KafkaTemplate")
